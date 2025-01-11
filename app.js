4f7ade8d8601d93a877a0536fac667f24feccc34
Jack Zheng <jackzheng274@gmail.com>
2025-01-10 23:43:40 -0800

    Update porta

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { Pool } = pkg;
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Set up EJS as the view engine
 app.set("view engine", "ejs");
