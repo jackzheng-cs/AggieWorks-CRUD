cc973b92da82260513af6d5b96b8a562b4b1376a
Jack Zheng <jackzheng274@gmail.com>
2025-01-11 20:07:49 -0800

    Set views directory

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const port = process.env.PORT || 3000;
 
 // Set up EJS as the view engine
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Middleware to serve static files and parse form data
 app.use(express.static("public"));
